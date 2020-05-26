9a96a94bf5e8023ebfdeef2f9c7faaf7abe1522f
John jonas <jonas.humenu@gmail.com>
2020-05-26 23:49:15 +0100

    Add UI

--- mytodo/forms.py
+++ mytodo/forms.py
@@ -3,6 +3,17 @@ from .models import *
 
 
 class TodoForm(forms.ModelForm):
-    class Meta:
+
+    class Meta(object):
         model = Todo
         fields = ['title']
+
+        widgets = {
+            'title': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'id': 'textiput',
+                    'placeholder': 'Enter Task here'
+                }
+            )
+        }
